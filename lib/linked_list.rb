d152d15183a5dbcb6b36f3a0210338a61248f20a
Baskakovs <baskakovs@me.com>
2023-04-12 16:10:54 +0100

    commit tree 986752d5026d851ec67f406998f0b3e2d78971e8

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,5 @@
 require_relative './node'
-
+require "byebug"
 class LinkedList
   attr_accessor :head, :tail
 
@@ -8,7 +8,43 @@ class LinkedList
     @tail = nil
   end
 
+  length = 0
+
   def reverse!
-    # your code here
+    if head.nil?
+      nil
+    else
+      length = 0
+      curr = head
+      while curr.next_node
+        length += 1
+        curr = curr.next_node
+      end
+      if length != 0
+        curr = tail
+        count = 0
+        while count < (length)
+          prev_to_tail = tail.prev_node
+          tail.prev_node = nil
+          prev_to_tail.next_node = nil
+          tail.next_node = head
+          head.prev_node = tail
+          # byebug
+          self.head = curr
+          self.tail = prev_to_tail
+          head.prev_node = nil 
+          count += 1
+        end
+      end
+    end
   end
 end
+# prev_to_tail = tail.prev_node
+# old_tail = tail
+# tail.prev_node = nil
+# prev_to_tail.next_node = nil
+# tail.next_node = head
+# head.prev_node = tail
+# self.head = old_tail
+# self.tail = prev_to_tail
+# count += 1
